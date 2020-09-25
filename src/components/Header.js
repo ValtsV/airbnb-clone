df11994f0857181217682359f13b351ab6e2e5eb
Valts Vilcans <valts03@inbox.lv>
2020-09-25 12:55:47 +0200

    Header comp

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.scss";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="header-icon"
+        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+        alt="logo"
+      />
+      <div className="header-center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+      <div className="header-right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
